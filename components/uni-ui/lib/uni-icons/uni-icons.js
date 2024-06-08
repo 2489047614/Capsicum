de84155b5d7fa6d13bb68d257ac3780ba80ff2a7
dermao <15687721774@163.com>
2024-06-08 20:09:46 +0800

    Initial Commit

---
+++ components/uni-ui/lib/uni-icons/uni-icons.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["components/uni-ui/lib/uni-icons/uni-icons"],{1251:function(n,t,e){"use strict";e.r(t);var u=e("13ab"),i=e("5311");for(var r in i)"default"!==r&&function(n){e.d(t,n,(function(){return i[n]}))}(r);e("5829");var c,o=e("f0c5"),a=Object(o["a"])(i["default"],u["b"],u["c"],!1,null,"cfd10468",null,!1,u["a"],c);t["default"]=a.exports},"13ab":function(n,t,e){"use strict";var u,i=function(){var n=this,t=n.$createElement;n._self._c},r=[];e.d(t,"b",(function(){return i})),e.d(t,"c",(function(){return r})),e.d(t,"a",(function(){return u}))},"178b":function(n,t,e){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u=i(e("2199"));function i(n){return n&&n.__esModule?n:{default:n}}var r={name:"UniIcons",props:{type:{type:String,default:""},color:{type:String,default:"#333333"},size:{type:[Number,String],default:16}},data:function(){return{icons:u.default}},methods:{_onClick:function(){this.$emit("click")}}};t.default=r},5311:function(n,t,e){"use strict";e.r(t);var u=e("178b"),i=e.n(u);for(var r in u)"default"!==r&&function(n){e.d(t,n,(function(){return u[n]}))}(r);t["default"]=i.a},5829:function(n,t,e){"use strict";var u=e("f632"),i=e.n(u);i.a},f632:function(n,t,e){}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'components/uni-ui/lib/uni-icons/uni-icons-create-component',
+    {
+        'components/uni-ui/lib/uni-icons/uni-icons-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("1251"))
+        })
+    },
+    [['components/uni-ui/lib/uni-icons/uni-icons-create-component']]
+]);
